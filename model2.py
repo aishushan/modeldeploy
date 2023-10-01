7a850d45f77b100a16b27c586da466f265f40af6
Aiswarya S <134680109+aishushan@users.noreply.github.com>
2023-10-01 22:48:57 +0530

    Update model2.py

--- model2.py
+++ model2.py
@@ -1,26 +1,12 @@
 import streamlit as st
 import numpy as np
 import librosa
-from sklearn.preprocessing import StandardScaler
+#from sklearn.preprocessing import StandardScaler
 from keras.models import load_model
 
 # Load the pre-trained model
 model = load_model('samplemodel.h5')
 
-# Define a Streamlit SessionState to persist the scaler
-class SessionState:
-    def __init__(self):
-        self.scaler = None
-
-# Initialize the session state
-state = SessionState()
-
-# Function to define and load the scaler
-def load_scaler():
-    scaler = StandardScaler()
-    scaler.mean_ = np.load('model2.npy')
-    scaler.scale_ = np.load('model2.npy')
-    return scaler
 
 # Function to extract MFCC features from an audio file
 def extract_mfcc(wav_file_name, scaler):
@@ -30,8 +16,8 @@ def extract_mfcc(wav_file_name, scaler):
     mel = np.mean(librosa.feature.melspectrogram(y=y, sr=sr).T, axis=0)
     
     # Normalize the features using the loaded scaler
-    audio_features = np.hstack((mfccs, chroma, mel))
-    audio_features = scaler.transform(audio_features.reshape(1, -1))
+    #audio_features = np.hstack((mfccs, chroma, mel))
+    #audio_features = scaler.transform(audio_features.reshape(1, -1))
     
     return audio_features
 
@@ -43,9 +29,7 @@ audio_file = st.file_uploader("Upload an audio file (WAV format)", type=["wav"])
 
 if audio_file is not None:
     try:
-        # Load the scaler if not already loaded
-        if state.scaler is None:
-            state.scaler = load_scaler()
+       
 
         # Extract MFCC features
         audio_features = extract_mfcc(audio_file, state.scaler)
